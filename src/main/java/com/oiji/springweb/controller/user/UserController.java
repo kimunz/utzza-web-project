770209814fa3f08e57b504ee9489bc327fba402c
Eunji Kim <oijkt7@gmail.com>
2022-05-30 23:11:30 +0900

    회원가입 Validation 유효성 검증

--- src/main/java/com/oiji/springweb/controller/user/UserController.java
+++ src/main/java/com/oiji/springweb/controller/user/UserController.java
@@ -5,7 +5,11 @@ import com.oiji.springweb.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 
 @Slf4j
@@ -31,12 +35,20 @@ public class UserController {
     }
 
     @GetMapping("/signUp")
-    public String signUpForm() {
+    public String signUpForm(Model model) {
+
+        model.addAttribute("user", new User());
         return "user/signUp";
     }
 
     @PostMapping("/signUp")
-    public String signUp(@ModelAttribute User user) {
+    public String signUp(@Valid @ModelAttribute User user, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "user/signUp";
+        }
+
         userService.save(user);
         return "redirect:/login";
     }
--- src/main/java/com/oiji/springweb/dto/user/User.java
+++ src/main/java/com/oiji/springweb/dto/user/User.java
@@ -4,21 +4,33 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
-@Getter
-@Setter
+@Data
 public class User {
 
-    @NotEmpty
+    public User() {
+    }
+
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+    @Pattern(regexp = "^[a-z0-9_]{4,12}$", message = "아이디는 4~12자 영문 소문자, 숫자를 사용하세요.")
     private String loginId;
-    @NotEmpty
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+                message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
+
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9-_]{2,8}$", message = "닉네임은 특수문자를 제외한 2~8자리여야 합니다.")
     @NotEmpty
     private String name;
-    @NotEmpty
+
+    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
-    @NotEmpty
+
     private String auth;
 
     public User(String loginId, String password, String name, String email, String auth)  {
